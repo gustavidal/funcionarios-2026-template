d400ae9c10b9dd5184916ecd940e038343fefec2
GUSTAVO VIDAL DE ABREU <25203686@CFP127ALUNO.LOCAL>
2026-04-07 14:01:25 -0300

    feat: finalizar projeto

--- app.js
+++ app.js
@@ -2,3 +2,25 @@
 
 import funcionarios from "./funcionarios.json" with { type: "json" }
 
+function criarCard(funcionario) {
+    const card = document.createElement('div')
+    card.className = 'card'
+
+    const foto = document.createElement('img')
+    foto.src = `./img/${funcionario.imagem}`
+    foto.alt = `Foto de ${funcionario.nome}`
+
+    const nome = document.createElement('h3')
+    nome.textContent = funcionario.nome
+
+    const cargo = document.createElement('span')
+    cargo.textContent = funcionario.cargo
+
+    card.append(foto, nome, cargo)
+
+    return card
+}
+
+const cards = funcionarios.map(criarCard)
+
+document.getElementById('container').replaceChildren(...cards)
